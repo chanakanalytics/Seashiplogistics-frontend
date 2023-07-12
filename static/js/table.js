f96a660f74e85140117c79ba18c7d55b3c74875a
KR!$$H <48906449+KrisshRp@users.noreply.github.com>
2023-07-12 16:45:01 +0530

    table button update

--- static/js/table.js
+++ static/js/table.js
@@ -1,28 +1,42 @@
-$(document).ready(function() {
-    var shipmentTable = $('#shipmentTable').DataTable( {
+$(document).ready(function () {
+    var shipmentTable = $('#shipmentTable').DataTable({
         lengthChange: false,
-        buttons: [{
-            extend: 'collection',
-            text: '<i class="fas fa-file-export"></i>',
-            buttons: [
-                'excel', 'pdf', 'print', 'copy'
-            ]
-        }]
-    } );
+        buttons: [
+            {
+                extend: 'collection',
+                text: '<i class="fas fa-file-export"></i>',
+                buttons: [
+                    'excel', 'pdf', 'print', 'copy'
+                ]
+            }
+        ]
+    });
     shipmentTable.buttons().container()
-        .appendTo( '#shipmentTable_wrapper .col-md-6:eq(0)');
+        .appendTo('#shipmentTable_wrapper .col-md-6:eq(0)');
 });
-$(document).ready(function() {
-    var masterTable = $('#masterTable').DataTable( {
+$(document).ready(function () {
+    var masterTable = $('#masterTable').DataTable({
         lengthChange: false,
-        buttons: [{
-            extend: 'collection',
-            text: '<i class="fas fa-file-export"></i>',
-            buttons: [
-                'excel', 'pdf', 'print', 'copy'
-            ]
-        }]
-    } );
+        buttons: [
+            {
+                extend: 'collection',
+                text: '<i class="fas fa-file-export"></i>',
+                buttons: [
+                    'excel', 'pdf', 'print', 'copy'
+                ]
+            },
+            {
+                text: '<i class="fas fa-plus-square"></i>',
+                className: "ms-2 add-button",
+            },
+            {
+                text: '<i class="fas fa-trash-alt"></i>',
+                className: "ms-2 edit-button",
+            }
+        ]
+    });
     masterTable.buttons().container()
-        .appendTo( '#masterTable_wrapper .col-md-6:eq(0)');
-} );
+        .appendTo('#masterTable_wrapper .col-md-6:eq(0)');
+    masterTable.buttons(".add-button").nodes().attr({"data-bs-toggle": "modal", "data-bs-target":"#addmodal"})
+    masterTable.buttons(".edit-button").nodes().attr({"data-bs-toggle": "modal", "data-bs-target":"#editmodal"})
+});
